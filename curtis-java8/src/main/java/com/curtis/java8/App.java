18340ba60a23c888433ca165529f250c1a3e0ce9
curtis <375577020@qq.com>
2021-09-10 01:30:26 +0800

    init project

---
+++ curtis-java8/src/main/java/com/curtis/java8/App.java
@@ -0,0 +1,13 @@
+package com.curtis.java8;/**
+/**
+ * @author curtis.cai
+ * @desc Java8新特性的使用示例
+ * @date 2021-09-10
+ * @email curtis.cai@outlook.com
+ * @reference
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
