5ba620b4631564a8efd0676aa9d29dc0a0ad8eec
curtis <375577020@qq.com>
2021-11-25 00:20:09 +0800

    Apache Api的使用示例

--- curtis-apache/src/main/java/com/curtis/apache/lang3/string/StringUtilsTest.java
+++ curtis-apache/src/main/java/com/curtis/apache/lang3/string/StringUtilsTest.java
@@ -1,5 +1,7 @@
 package com.curtis.apache.lang3.string;
 
+import com.google.common.collect.Lists;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
 /**
@@ -13,6 +15,12 @@ public class StringUtilsTest {
 
     @Test
     public void testStringUtils(){
+        boolean isNotBlank = StringUtils.isNotBlank(null);
+        boolean isNotEmpty = StringUtils.isNotEmpty(null);
+
+        String[] split = StringUtils.split("a&b", "&");
+
+        String join = StringUtils.join(Lists.newArrayList("a", "b", "c"), "&");
 
     }
 }
